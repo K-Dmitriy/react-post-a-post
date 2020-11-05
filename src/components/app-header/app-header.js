2e8a36438406423092b6d1167afddb9b96c4e1eb
d.kozlov <d.kozlov@hottelecom.by>
2020-11-05 17:44:08 +0300

    add structure application, add styles

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Dmitry Kozlov</h1>
+            <h2>5 записей, из них понравилось 0 записей</h2>
+        </div>
+    );
+};
+
+export default AppHeader;
